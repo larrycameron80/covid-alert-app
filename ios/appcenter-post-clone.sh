460705c526d7e124a1555a87f9f8e0c84a3dfa16
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-07-14 21:35:27 -0400

    generate build .env

---
+++ ios/appcenter-post-clone.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+cd ..
+dotenv=./.env
+touch $dotenv
+
+echo "APP_ID_IOS=$APP_ID_IOS" >> $dotenv
+echo "APP_VERSION_NAME=$APP_VERSION_NAME" >> $dotenv
+echo "APP_VERSION_CODE=$APP_VERSION_CODE" >> $dotenv
+
+echo "SUBMIT_URL=$SUBMIT_URL" >> $dotenv
+echo "RETRIEVE_URL=$RETRIEVE_URL" >> $dotenv
+
+echo "HMAC_KEY=$HMAC_KEY" >> $dotenv
+echo "SENTRY_DSN=$SENTRY_DSN" >> $dotenv
+
+echo "TEST_MODE=$TEST_MODE" >> $dotenv
+echo "MOCK_SERVER=$MOCK_SERVER" >> $dotenv
+
+echo "MCC_CODE=$MCC_CODE" >> $dotenv
+echo "TRANSMISSION_RISK_LEVEL=$TRANSMISSION_RISK_LEVEL" >> $dotenv
+echo "MINIMUM_FETCH_INTERVAL=$MINIMUM_FETCH_INTERVAL" >> $dotenv
+
+cat $dotenv
